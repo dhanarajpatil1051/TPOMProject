4516ffbf775935e27a539f9563dc3da5596a3854
dhanarajpatil1051 <100409850+dhanarajpatil1051@users.noreply.github.com>
2022-02-26 14:07:31 +0530

    Update applicationcounts-reports-details.php
    applications counts reports details

--- company/applicationcounts-reports-details.php
+++ company/applicationcounts-reports-details.php
@@ -13,7 +13,7 @@ if (strlen($_SESSION['crmscid']==0)) {
 <html lang="zxx">
 <head>
     
-    <title>Campus Recruitment Management System-Application Counts Reports</title>
+    <title>Training and Placement Management System-Application Counts Reports</title>
     <!-- CSS -->
     <link rel="stylesheet" href="assets/css/app.css">
     <style>
