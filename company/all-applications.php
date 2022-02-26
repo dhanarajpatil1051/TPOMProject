1c9ec1f7d98caf1a785f4b155adb97632a3771fd
dhanarajpatil1051 <100409850+dhanarajpatil1051@users.noreply.github.com>
2022-02-26 14:04:36 +0530

    Update all-applications.php
    all applications

--- company/all-applications.php
+++ company/all-applications.php
@@ -13,7 +13,7 @@ if (strlen($_SESSION['crmscid']==0)) {
 <html lang="zxx">
 <head>
     
-    <title>Campus Recruitment Management System-Selected Applications</title>
+    <title>Training and Placement Management System-Selected Applications</title>
     <!-- CSS -->
     <link rel="stylesheet" href="assets/css/app.css">
     <style>
