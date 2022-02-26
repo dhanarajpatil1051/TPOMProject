28c2c89617fb02b87c72bd3dba0a6410440bdaac
dhanarajpatil1051 <100409850+dhanarajpatil1051@users.noreply.github.com>
2022-02-26 14:17:49 +0530

    Update manage-vacancy.php

--- company/manage-vacancy.php
+++ company/manage-vacancy.php
@@ -13,7 +13,7 @@ if (strlen($_SESSION['crmscid']==0)) {
 <html lang="zxx">
 <head>
     
-    <title>Campus Recruitment Management System-Manage Vacancy</title>
+    <title>Training and Placement Management System-Manage Vacancy</title>
     <!-- CSS -->
     <link rel="stylesheet" href="assets/css/app.css">
     <style>
