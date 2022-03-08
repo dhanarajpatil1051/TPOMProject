2d36841fe3f1302fffba76b3084c2e5c2875ff73
abhishekmali4402 <100214278+abhishekmali4402@users.noreply.github.com>
2022-03-08 16:26:54 +0530

    Update histroy-applied-job.php

--- user/histroy-applied-job.php
+++ user/histroy-applied-job.php
@@ -13,7 +13,7 @@ if (strlen($_SESSION['crmsuid']==0)) {
 <html lang="zxx">
 <head>
     
-    <title>Campus Recruitment Management System-History of applied Jobs</title>
+    <title>Training and Placement Management System-History of applied Jobs</title>
     <!-- CSS -->
     <link rel="stylesheet" href="assets/css/app.css">
     <style>
